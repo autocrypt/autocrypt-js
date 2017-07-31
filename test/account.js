2b93b73c696c91f1dd10e13771b83f3adbf03bee
karissa <krmckelv@gmail.com>
2017-07-31 16:44:38 -0700

    add test harness

--- test/account.js
+++ test/account.js
@@ -5,6 +5,7 @@ var test = require('tape')
 var email = 'hello@xyz.net'
 
 test('throw error if account does not exist', function (t) {
+  var crypt = autocrypt()
   crypt.getAccount(email, (err, account) => {
     t.ok(err)
   })
